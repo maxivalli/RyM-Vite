63cc1933f68178bb554a88507906bacfec334dd2
maxivalli <maximilianovalli.sc@gmail.com>
2023-08-29 16:25:21 -0300

    v1.6

--- src/views/favorites/Favorites.jsx
+++ src/views/favorites/Favorites.jsx
@@ -1,10 +1,13 @@
 import { useSelector, useDispatch } from "react-redux";
-import { orderFavorites, filterFavorites, resetFavorites } from "../../redux/actions";
+import {
+  orderFavorites,
+  filterFavorites,
+  resetFavorites,
+} from "../../redux/actions";
 import Cards from "../../components/Cards/Cards";
 import style from "./Favorites.module.css";
 
 export default function Favorites() {
-  
   const favorites = useSelector((state) => state.myFavorites);
   const dispatch = useDispatch();
 
@@ -15,7 +18,7 @@ export default function Favorites() {
   function handleFilter(e) {
     dispatch(filterFavorites(e.target.value));
   }
-  
+
   function handleReset() {
     dispatch(resetFavorites());
   }
@@ -24,21 +27,30 @@ export default function Favorites() {
     <>
       <div className={style.selectors}>
         <select onChange={handleFilter}>
-        <option value="" disabled>Genero</option> 
+          <option disabled selected value="">
+            Género
+          </option>
           {["Male", "Female", "unknown", "Genderless"].map((gender) => (
-            <option key={gender} value={gender}>{gender}</option>
+            <option key={gender} value={gender}>
+              {gender}
+            </option>
           ))}
         </select>
         <select onChange={handleSort}>
-        <option value="" disabled>Orden</option> 
+          <option disabled selected value="">
+            Orden
+          </option>
           {["Ascendente", "Descendente"].map((order) => (
-            <option key={order} value={order}>{order}</option>
+            <option key={order} value={order}>
+              {order}
+            </option>
           ))}
         </select>
-        <button onClick={handleReset} className={style.reset}>RESET</button>
+        <button onClick={handleReset} className={style.reset}>
+          RESET
+        </button>
       </div>
-      <Cards characters={favorites} className={style.cards}/>
+      <Cards characters={favorites} className={style.cards} />
     </>
   );
 }
-
