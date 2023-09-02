c9349f98fd407d109d2d7d91decbbf07af532833
maxivalli <maximilianovalli.sc@gmail.com>
2023-09-01 21:39:05 -0300

    v1.8.2

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,18 +1,31 @@
 import { Link } from "react-router-dom";
 import Card from "../Card/Card";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import style from "./Cards.module.css";
 
 export default function Cards(props) {
   const { characters, onClose } = props;
 
+  const sliderSettings = {
+    dots: true,
+    infinite: false, 
+    speed: 500,
+    slidesToShow: Math.min(6, characters.length), 
+    slidesToScroll: 6,
+  };
+
   return (
     <>
     <div className={style.story}>
+      <Slider {...sliderSettings} className={style.slide}>
         {characters.map((character) => (
           <Link to={`/image/${character.id}`} key={character.id}>
-            <img src={character.image} alt={`Character ${character.id}`} />
+            <img src={character.image} alt={`Character ${character.id}`}/>
           </Link>
         ))}
+        </Slider>
       </div>
       <div className={style.mainContainer}>
         {characters.map((character) => (
