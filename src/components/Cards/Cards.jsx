e5d5e36afbb33002a38ee1dfbd0f19ecd679a66e
maxivalli <maximilianovalli.sc@gmail.com>
2023-09-13 19:59:22 -0300

    v3.1

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,4 +1,6 @@
 import Card from "../Card/Card";
+import morty from "../../assets/morty.png"
+import rick from "../../assets/rick.png"
 import style from "./Cards.module.css";
 
 export default function Cards(props) {
@@ -6,6 +8,8 @@ export default function Cards(props) {
 
   return (
     <>
+    <img src={morty} className={style.morty}></img>
+    <img src={rick} className={style.rick}></img>
       <div className={style.container}>
         {characters.map((character) => (
           <Card key={character.id} character={character} characters={characters} setCharacters={setCharacters} button={button} />
