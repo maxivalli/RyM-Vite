0199b72caf79448fb12f16cb18af9b954ddef0dc
maxivalli <maximilianovalli.sc@gmail.com>
2023-09-14 02:37:55 -0300

    v3.2

--- src/views/landingPage/LandingPage.jsx
+++ src/views/landingPage/LandingPage.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Form from '../../components/Form/Form'
 import style from './LandingPage.module.css'
 import Logo from '../../assets/logo.png'
+import morty from '../../assets/morty.gif'
 
 export default function LandingPage({ login }) {
   return (
     <>
+    <img src={morty} className={style.morty}></img>
     <div className={style.logo}>
         <img src={Logo}></img>
         <h3 className={style.by}>By Maxi Valli</h3>
