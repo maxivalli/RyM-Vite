21556a65ce6ad9d3a814b3e30afa5049f8ffdae0
maxivalli <maximilianovalli.sc@gmail.com>
2023-09-18 16:40:34 -0300

    v3.5

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -1,34 +1,57 @@
 import React from "react";
 import style from "./About.module.css";
 import Perfil from "../../assets/perfil.png";
-import Instagram from '../../assets/instagram.png';
-import Gmail from '../../assets/gmail.png';
-import Github from '../../assets/github.png'
+import Instagram from "../../assets/instagram.png";
+import Gmail from "../../assets/gmail.png";
+import Github from "../../assets/github.png";
 
 export default function About() {
   return (
     <>
-    <div className={style.aboutContainer}>
-      <h2>ABOUT ME</h2>
-      <div className={style.perfil}>
-        <img src={Perfil}></img>
-      </div>
-      <div className={style.text}>
-        <p>
-          Mi nombre es Maximiliano Valli, tengo 38 años y soy de la provincia de Santa Fe,<br></br>
-          empecé este año a estudiar en Henry porque tenía ganas de aprender
-          programación.<br></br>
-          <br></br>
-          Hasta ahora la expericia es genial y esta página fue construída con
-          todo lo que aprendí.
-        </p>
-        <div>
-          <a href='https://www.instagram.com/maxi.valli.aicardi/' target="_blank"> <img src={Instagram} alt="instagram" className={style.logo}/> </a>
-          <a href='mailto:maximilianovalli.sc@gmail.com'> <img src={Gmail} alt="gmail" className={style.logo}/> </a>
-          <a href='https://github.com/maxivalli' target="_blank"> <img src={Github} alt="github" className={style.logo}/> </a>
+      <div className={style.aboutContainer}>
+        <h2>ABOUT ME</h2>
+        <div className={style.perfil}>
+          <img src={Perfil}></img>
+        </div>
+        <div className={style.text}>
+          <p>
+            Soy Maximiliano Valli, un estudiante apasionado de programación en
+            HENRY. Mi experiencia abarca React, Redux, JavaScript, CSS, HTML,
+            Node.js y SQL.
+            <br />
+            <br />
+            Mi último proyecto es una página web basada en la API de Rick and
+            Morty. Te permite explorar y obtener información detallada de los
+            personajes buscándolos por su ID.
+            <br />
+            <br />
+            Mi objetivo es brindarte una experiencia intuitiva y entretenida.
+            ¡Gracias por visitar mi sitio y únete a mí en esta emocionante
+            aventura tecnológica.
+          </p>
+          <div>
+            <a
+              href="https://www.instagram.com/maxi.valli.aicardi/"
+              target="_blank"
+            >
+              {" "}
+              <img
+                src={Instagram}
+                alt="instagram"
+                className={style.logo}
+              />{" "}
+            </a>
+            <a href="mailto:maximilianovalli.sc@gmail.com">
+              {" "}
+              <img src={Gmail} alt="gmail" className={style.logo} />{" "}
+            </a>
+            <a href="https://github.com/maxivalli" target="_blank">
+              {" "}
+              <img src={Github} alt="github" className={style.logo} />{" "}
+            </a>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
